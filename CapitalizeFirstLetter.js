a35fe358ff6f81d2f221ce4f1311255536e8e0cd
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-12-13 18:52:59 -0800

    Add - CapitalizeFirstLetter

---
+++ CapitalizeFirstLetter.js
@@ -0,0 +1,16 @@
+/**********************************
+Coding Challenge:
+    Capitalize the first letter of each word from the sentence: "freeCodeCamp is an awesome resource."
+*/
+
+/**********************************
+Solution
+*/
+
+let mySentence = "freeCodeCamp is an awesome resource.";          // Assign string to a variable so can work with it
+let words = mySentence.split(" ");                                // Split sentence into array so we can manipulate each individually
+
+for (let i = 0; i < words.length; i++) {                          // Loop thru words array using for loop ...
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1)     //... and for each item, capitalize first letter + remainder of string
+}
+words.join(" ");        // Join all recently-muted items into a string
