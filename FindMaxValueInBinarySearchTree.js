0d2ee652b25c6123aa3775762baddf66faa4ef56
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-02-23 21:09:33 -0800

    Update formatting

--- FindMaxValueInBinarySearchTree.js
+++ FindMaxValueInBinarySearchTree.js
@@ -1,6 +1,10 @@
-// Coding Challenge: Find the maximum/largest value in a binary search tree. 
+/**********************************
+Coding Challenge: Find the maximum/largest value in a binary search tree. 
+*/
 
-// Solution:
+/**********************************
+Solution:
+*/
 class Node {                            
     constructor(value) {
         this.value = value;
