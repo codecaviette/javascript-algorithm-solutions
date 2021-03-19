67ce8262680ac0a3a35a130a76be55dcee207a4f
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-03-18 19:29:29 -0700

    Add solution to CountCharacterOccurances

--- CountCharacterOccurances.js
+++ CountCharacterOccurances.js
@@ -1,7 +1,36 @@
 /**********************************
-Coding Challenge:
+Coding Challenge: 
+    Count the number of times each character occurs in a string.
+
+    Input: var string = "apple";
+    Output:
+        a:1
+        p:2
+        l:1
+        e:1
 */
 
 /**********************************
 Solution:
 */
+
+// 1. Pseudocode:
+// Convert string to an array so I can manipulate it
+// Loop thru array and create a new object, adding key:value pairs as you go
+  // Need to first add the object key with value 0 so you can then increment the value
+  // If the loop finds the same key, it will simply iterate the value of that key
+
+// 2. Code: 
+function countCharacterOccurances(str) {
+    let strArray = str.split("");                    // Converts string to an array separating by each character
+    let strObject = {};                              // We'll fill this object w key:value pairs of letter:count from the array
+
+    for (let i=0; i < strArray.length; i++) {        // Loop thru array...
+        if (strObject[strArray[i]] == undefined)     // and if the obj doesn't have a key of strArray[i]...
+            strObject[strArray[i]] = 0;              // ...then create an obj key of strArray[i] with value of 0
+            strObject[strArray[i]]++ ;               // Now that the key exists, increment its value
+        }
+    return strObject;
+}
+
+countCharacterOccurances('apple');        // Output: {a: 1, p: 2, l: 1, e: 1}
