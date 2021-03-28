d757379fea7af0e5e33c59c0ced5da21870b06d2
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-03-27 21:15:56 -0700

    Add challenge to StringToCountedString

--- StringToCountedString.js
+++ StringToCountedString.js
@@ -1,5 +1,7 @@
 /**********************************
 Coding Challenge: 
+    let str = “aabcaa”;
+    On run time, write a fxn to get alphabetical order and # times it appears:  let str = “a4b2c1"
 */
 
 /**********************************
