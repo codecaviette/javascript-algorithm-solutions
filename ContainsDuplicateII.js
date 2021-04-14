2d169b74f7b0a3ecce90b44736452c2326286c00
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-04-13 21:29:55 -0700

    Add solution A to ContainsDuplicateII

--- ContainsDuplicateII.js
+++ ContainsDuplicateII.js
@@ -15,5 +15,28 @@ Given an integer array nums and an integer k, return true if there are two disti
 */
 
 /**********************************
-Solution:
+Solutions:
 */
+
+// Solution A (brute force, 2 nested loops, more expensive)
+  // 1. Pseudocode:
+    // absolute value of a value:  Math.abs(value)
+
+    // loop thru i=0
+        // loop thru j=i+1
+            // if nums[i] == nums[j] && Math.abs(i-j) <= k
+                // return true
+        // otherwise, return false
+  // 2. Code:
+  var containsNearbyDuplicate = function(nums, k) {
+      for (let i=0; i< nums.length; i++) {
+          for (let j=i+1; j<nums.length; j++) {
+              if (nums[i] == nums[j] && (Math.abs(i-j) <= k)) {
+                  return true;
+              }
+          }
+      } return false;
+  };
+  
+  // 3. Time complexity: O(n^2) bc 2 nested loops
+     // Space comp: O(1) ? since no objects are being created?
