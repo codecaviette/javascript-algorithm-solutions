98093375b64f08129fbbd57be9e875bfbe95aea1
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-04-19 21:05:22 -0700

    Add pseudocode to CountVowelsInString

--- CountVowelsInString.js
+++ CountVowelsInString.js
@@ -6,3 +6,8 @@ Coding Challenge:
 /**********************************
 Solution:
 */ 
+
+// 1. Pseudocode:
+    // loop through string and count each vowel
+    // define vowels
+    // define counter that will begin empty and we will fill later
