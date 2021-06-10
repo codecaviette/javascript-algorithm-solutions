75aee50d6da87b0d129966a740b6d9185366e1c8
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-06-10 16:36:58 -0700

    Add psuedocode

--- ReturnAllButFirstLetter.js
+++ ReturnAllButFirstLetter.js
@@ -5,3 +5,5 @@ Coding Challenge:   Return all but the first letter of a word.
 /**********************************
 Solution:
 */
+
+// Use .substring method to return a new string only including characters between two indices  
