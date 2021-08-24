e6e10c7d423992a1b5776ba451694dbd21b38186
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-08-24 16:06:04 -0700

    Add prompt to CountDownWithRecursion

--- CountDownWithRecursion.js
+++ CountDownWithRecursion.js
@@ -1,5 +1,12 @@
 /**********************************
-Coding Challenge: 
+Coding Challenge: Given an integer, count down to 1 by logging each integer along the way.
+
+Ex -  Input: 10
+      Output:   // 10
+                // 9
+                // 8
+                // ...
+                // 1
 */
 
 
