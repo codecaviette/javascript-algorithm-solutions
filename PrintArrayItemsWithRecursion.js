2aa72d8e832c33c4f09d8e42bf2f2dc062561f7b
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-09-07 16:16:02 -0700

    Add solution to printarrayitemswithrecursion

--- PrintArrayItemsWithRecursion.js
+++ PrintArrayItemsWithRecursion.js
@@ -6,3 +6,19 @@ Coding Challenge: Go through an array and print out all of the elements through 
 /**********************************
 Solution:
 */
+
+function print(array) {
+    // Base case
+    if (array.length < 2) return array;         // When array length is empty or 1, return current array value
+  
+    // Recursive call
+    console.log(array[0]);                      // as long as array length is 2 or greater, log the first item...
+    print(array.slice(1));                      // ... and call the print function again but this time passing everything except the array's first item
+  
+  }
+  
+  print([1, 2, 3,4,5]);         // logs 1
+                                    //  2
+                                    //  3
+                                    //  4
+  
