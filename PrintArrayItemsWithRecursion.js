b6f6a08d74984f855a740b4ffb672c16986cf174
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-09-11 22:01:27 -0700

    Fix formatting of coding challenge in printarrayitemswithrecursion

--- PrintArrayItemsWithRecursion.js
+++ PrintArrayItemsWithRecursion.js
@@ -1,6 +1,5 @@
 /**********************************
 Coding Challenge: Go through an array and print out all of the elements through a recursive solution
-
 */
 
 /**********************************
