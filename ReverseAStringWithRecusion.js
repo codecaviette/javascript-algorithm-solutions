a1ce6526096df0a601a71aaa6af8fa26f35a406c
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-09-16 16:03:32 -0700

    Add solution to revereastringwithrecursion

--- ReverseAStringWithRecusion.js
+++ ReverseAStringWithRecusion.js
@@ -6,3 +6,13 @@ Coding Challenge: Write a recursive function that will return a reversed copy of
 /**********************************
 Solution
 */
+
+function reverse(str) {
+  // Base case
+  if (str.length < 2) return str;
+
+  // Recursive call
+  return reverse(str.slice(1, str.length)) + str[0];        // Process ENTIRE line/return stmt each time recursive call is made (including str[0])
+}
+
+reverse('apple');
