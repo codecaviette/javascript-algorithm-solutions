5ea4ccc3f6b4709ffb54c1ac7ae5eaa7c8bb008a
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-09-30 16:32:16 -0700

    Add prompt to pairssumtotargetnumber file

--- PairsSumToTargetNumber.js
+++ PairsSumToTargetNumber.js
@@ -1,5 +1,14 @@
 /**********************************
-Coding challenge
+Coding challenge: 
+Take array and find the pairs of that add up to 10 (or a target number)
+
+let array = [1,4,5,2,7,6,3];
+
+3 Time complexities possible - solve for each:
+N^2
+N LogN
+N
+
 */
 
 /**********************************
