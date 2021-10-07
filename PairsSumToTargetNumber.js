6cfb34c3933b2f5435b4d0e2d033ede335f27149
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-10-06 20:33:38 -0700

    Update formatting of coding challenge

--- PairsSumToTargetNumber.js
+++ PairsSumToTargetNumber.js
@@ -1,14 +1,12 @@
 /**********************************
-Coding challenge: 
-Take array and find the pairs of that add up to 10 (or a target number)
+Coding challenge:  Take array and find the pairs of that add up to 10 (or a target number)
 
 let array = [1,4,5,2,7,6,3];
 
-3 Time complexities possible - solve for each:
-N^2
-N LogN
-N
-
+3 possible time complexities - solve for each:
+- N^2
+- N LogN
+- N
 */
 
 /**********************************
