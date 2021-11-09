bccdfa43c494138e63a1e7f0d4a24939f51f102c
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-11-08 20:16:20 -0800

    Add challenge to productofarrayitems file

--- ProductOfArrayItems.js
+++ ProductOfArrayItems.js
@@ -1,5 +1,6 @@
 /**********************************
 Coding challenge:
+Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
 */
 
 /**********************************
