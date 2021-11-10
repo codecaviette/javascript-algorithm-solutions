e0c890ee538e20a268f72894e9f5e7e8849a3055
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-11-09 20:35:31 -0800

    Add practice.js file

---
+++ practice.js
@@ -0,0 +1,9 @@
+// To run code, enter "node practice.js" into terminal
+
+let printLetters = (word) => {
+    for(let i=0; i<word.length; i++) {
+        console.log(word[i]);
+    }
+}
+
+printLetters("hello");
