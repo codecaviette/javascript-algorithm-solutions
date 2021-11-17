492bb899fb498d10157e1a9a508f0e1f3c4b1251
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-11-17 15:45:24 -0800

    Add solution to productofarrayitems

--- ProductOfArrayItems.js
+++ ProductOfArrayItems.js
@@ -14,3 +14,16 @@ Solution:
 // Pseudocode:
 // loop through both levels of array
 // multiply product of each subarray
+
+function multiplyAll(arr) {
+    var product = 1;                                    // declare var equal to initial val which we'll fill w fxn
+
+    for (var i = 0; i < arr.length; i++) {              // i represents the top layer of iteration where first array item = [1,2]
+        for (var j = 0; j < arr[i].length; j++) {       // loop through each item in subarrays; j represents 2nd layer of iteration where first item is the 1 of [1,2]
+            product = product * arr[i][j];              // set product equal to product * iterated subarray item
+        }
+    }
+    return product;
+}
+
+multiplyAll([[1,2],[3]]);         // output: 6
