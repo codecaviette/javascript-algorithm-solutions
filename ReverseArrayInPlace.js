a483e4baa07cfe49538919f8ed81eacdf590ddaf
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2021-12-12 13:13:08 -0800

    Add example to reversearrayinplace

--- ReverseArrayInPlace.js
+++ ReverseArrayInPlace.js
@@ -1,7 +1,9 @@
 /**********************************
 Coding challenge:
-Given an array, reverse items in place...
+Given an array, reverse items in place.
 
+Ex: input: [1,2,3,4,5]
+    output:[5,4,3,2,1]
 */  
 
 
