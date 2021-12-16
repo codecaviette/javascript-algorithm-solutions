d6255a9f16cba29f342eee76546b756f2fd839e5
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2021-12-16 10:05:44 -0800

    Add example to reversearrayinplace

--- ReverseArrayInPlace.js
+++ ReverseArrayInPlace.js
@@ -2,8 +2,11 @@
 Coding challenge:
 Given an array, reverse items in place.
 
-Ex: input: [1,2,3,4,5]
+Ex1 input: [1,2,3,4,5]
     output:[5,4,3,2,1]
+
+Ex2 Input: [2,4,6,8]
+    Output: [8,6,4,2]
 */  
 
 
--- practice.js
+++ practice.js
@@ -1,11 +1,51 @@
 // To run code, enter "node practice.js" into terminal
+// Code may not return value; instead, use console.log to view result
+
+/*
+Example Input: [-1, 2, 3, 1, -3, 2]
+Subarray Size: 2
+
+find max sum of subarray w length of 2
+sliding window approach
+
+track: leftIndex, rightIndex, currSum, maxSum (in this case, left and right start at 0)
+while rightIndex is less than array length (we want to stop calculating when the rightIndex reaches end of array
+  ... while always keeps track of which index you want to track/will iterate the most times)
+  calculate the sum of left and right indices === currSum
+  compare currSum and maxSum and assign larger value to maxSum
+  increment both left and right to loop again
+  while loop will exit once right is no longer less than array.length
+
+*/      
+
+function findMaxSum(arr) {
+  let leftIndex = 0,
+      rightIndex = 1,
+      maxSum = 0;
+
+  while (rightIndex < arr.length) {
+    let currSum = arr[leftIndex] + arr[rightIndex];
+    maxSum = Math.max(maxSum, currSum);
+    leftIndex++;
+    rightIndex++;
+  }
+  console.log(maxSum);
 
 
-function printBackwards(name) {
-  for (let i=name.length-1; i>=0; i--) {
-    console.log(name.charAt(i));
-}}
 
-let name = 'Jenny';
+  // let maxSum = 0;
+  
+  // for (let i = 0; i < arr.length-1; i++) {
+  //   let currSum = arr[i] + arr[i+1];
+  //   maxSum = Math.max(maxSum, currSum);
+  // }
+  // console.log(maxSum);
 
-printBackwards(name);
+}
+
+findMaxSum([2,4,6,8]);
+
+
+
+
+
