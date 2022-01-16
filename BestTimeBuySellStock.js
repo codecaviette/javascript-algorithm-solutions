cf83f937b108bb8ce0c8703296f8ab4e6c8c576e
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2022-01-16 08:56:28 -1000

    Add example

--- BestTimeBuySellStock.js
+++ BestTimeBuySellStock.js
@@ -1,11 +1,16 @@
 /**********************************
-Coding challenge:
-
-Prompt: Given an array of Apple stock prices from yesterday, write an efficient function that takes stockPrices and returns the best profit you could have made from one purchase and one sale of one share of Apple stock yesterday.
+Coding challenge: Given an array of Apple stock prices from yesterday, write an efficient function that takes stockPrices and returns the best profit you could have made from one purchase and one sale of one share of Apple stock yesterday.
 
 The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
 The values are the price (in US dollars) of one share of Apple stock at that time.
 So if the stock cost $500 at 10:30am, that means stockPrices[60] = 500.
+
+Ex:
+const stockPrices = [10, 7, 5, 8, 11, 9];
+
+getMaxProfit(stockPrices);
+// Returns 6 (buying for $5 and selling for $11)
+
 */
 
 /**********************************
