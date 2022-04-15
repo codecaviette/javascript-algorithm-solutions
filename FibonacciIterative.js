dbb919e76218659ffa07539e2304f773ecd22064
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2022-04-15 11:26:41 -0700

    Add Fib Iterative file and prompt

---
+++ FibonacciIterative.js
@@ -0,0 +1,12 @@
+/**********************************
+Coding Challenge: Given a number, n, generate the Fibonacci series up to nth term.
+
+Ex: Input: 4
+    Output: 0, 1, 1, 2
+
+*/
+
+/**********************************
+Solution:
+*/
+
