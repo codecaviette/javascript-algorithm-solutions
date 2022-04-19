4c91ab29ee3bb6140c305280f573defbc8c13f27
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2022-04-19 11:22:10 -0700

    Add solution to FibonacciIterative.js

--- FibonacciIterative.js
+++ FibonacciIterative.js
@@ -1,5 +1,6 @@
 /**********************************
-Coding Challenge: Given a number, n, generate the Fibonacci series up to nth term.
+Coding Challenge: Given a number, num, generate the Fibonacci series up to nth term. 
+                  Log each new term as you go.
 
 Ex: Input: 4
     Output: 0, 1, 1, 2
@@ -9,4 +10,22 @@ Ex: Input: 4
 /**********************************
 Solution:
 */
+// w fib, always need to set initial values which are 0,1 (sometimes ppl start at 1,1 - dn matter, just clarify w interviewer)
+// also nd a temporary place holder var to keep track of next fib value (sum of preceeding 2 items)
+// loop through 1 to num bc we're counting/printing the number of fib terms until we reach n
+    // before any operations, print first term 
 
+let fibToNthItem = (num) => {
+    let n1 = 0, 
+        n2 = 1, 
+        nextTerm;
+
+    for (let i = 1; i <= num; i++) {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+}
+
+fibToNthItem(5);
